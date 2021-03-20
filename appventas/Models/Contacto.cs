d5c8726505fb513153299327eaaea7c8a7535fa3
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-03-20 08:53:34 -0500

    App de Ventas

---
+++ appventas/Models/Contacto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace appventas.Models
+{
+    public class Contacto
+    {
+        public String FirstName {get; set;}
+        public String LastName {get; set;}
+        public String Email {get; set;}
+        public String Phone {get; set;}
+    }
+}
