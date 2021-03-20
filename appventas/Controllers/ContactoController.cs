790031f81073f867ddbf0ebeed6c3058bd8108aa
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-03-20 08:58:56 -0500

    Contacto Controller

---
+++ appventas/Controllers/ContactoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using appventas.Models;
+
+namespace appventas.Controllers
+{
+    public class ContactoController : Controller
+    {
+        private readonly ILogger<ContactoController> _logger;
+
+        public ContactoController(ILogger<ContactoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
